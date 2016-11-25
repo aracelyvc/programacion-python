7d1e2bddcc6aad94d45b3112a1f787b7f1a1c874
aracelyvc <aracelyvc@users.noreply.github.com>
2016-11-24 18:39:12 -0600

    Update ejercicio2.py

--- Laboratorio 1/ejercicio2.py
+++ Laboratorio 1/ejercicio2.py
@@ -1,6 +1,6 @@
 #Aracely Velazquez Calderon
-#Introduccion a la Programacion en python 2
-#22/11/2016
+#Introduccion a la Programacion en python
+#Laboratorio 1
 
 def intereses(x):
     sum=0
@@ -8,29 +8,20 @@ def intereses(x):
     sum=x+i
     return sum
 x = input()
-print intereses(x)
-
+print "en 1 año tu saldo sera:", intereses(x)
+print "ingresa nuevamente tu saldo inicial"
 def intereses(x):
     sum=0
     i=(2*x*0.04)
     sum=x+i
     return sum
 x = input()
-print intereses(x)
-
+print "en 2 año tu saldo sera:", intereses(x)
+print "ingresa nuevamente tu saldo inicial"
 def intereses(x):
     sum=0
     i=(3*x*0.04)
     sum=x+i
     return sum
 x = input()
-print intereses(x)
-
-
-interes_por_year=0.04
-saldo inicial=input("la cantidad a depositar")
-nvo_saldo=0
-for in range(4):
-        saldo=saldo_inicial*1.04
-    nvo_saldo=nvo_saldo+saldo
-    print nvo_saldo
+print "en 3 año tu saldo sera:", intereses(x)
