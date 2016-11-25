3bd8cb9addb7ae0afd05bcadd29ee30400462415
aracelyvc <aracelyvc@users.noreply.github.com>
2016-11-24 18:47:58 -0600

    Update ejercicio6.py

--- Laboratorio 1/ejercicio6.py
+++ Laboratorio 1/ejercicio6.py
@@ -1,23 +1,26 @@
+#Aracely Velazquez Calderon
+#Introduccion a la Programacion en Python
+#Laboratorio 1
 
-print "ingresa tu peso(kg) y altura(m), ambos con punto decimal, por ejemplo: imc(50.0,160.0)"
+print "ingresa tu peso(kg) y altura(m), ambos con punto decimal, por ejemplo: imc(50.0,1.60), depues de ello da enter y espera aque aparesca el reultado"
 def imc(p,h):
-    imc=(p/(h*h))
+    imc=p/(h*h)
     return imc
-print imc
+    print imc(p,h)
+print"luego que haya aparecido el resultado ingresa tu imc con punto decimal, ejemplo: x(23.0) y da enter"
 
-def clasificacion(imc):
-    if imc<16.0:
-        print "delgadez severa", calsificacion
-    elif imc in range(16.0,16.99):
-        print "delgadez moderada", clasificacion
-    elif imc in range(17.0,18.49):
-        print "delgadez leve", clasificacion
-    elif imc in range(18.5,24.99):
-        print "normal", clasificacion
-    elif imc<=25:
-        print "sobrepeso", clasificacion
-    elif imc<=30:
-        print "obesidad", clasificacion
-    elif imc<=40:
-        print "obesidad morbida", clasificacion
-print clasificacion
+def x(imc):
+    if imc < 16.0:
+        print "delgadez severa"
+    elif imc > 16.0 and imc < 16.99:
+        print "delgadez moderada"
+    elif imc > 17.0 and imc < 18.49:
+        print "delgadez leve"
+    elif imc > 18.5 and imc < 24.99:
+        print "normal"
+    elif imc <= 25:
+        print "sobrepeso"
+    elif imc <= 30:
+        print "obesidad"
+    elif imc <= 40:
+        print "obesidad morbida"
